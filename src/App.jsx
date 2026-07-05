dd2367539db42cb3280524fe0595da76ff7e341a
rookie-sn <kavinesh342003@gmail.com>
2026-07-05 17:31:29 +0530

    feat: Time and Hooks are added in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect, useRef } from 'react'
+import pillIcon from './assets/pill.png'
+import Header from './componenets/Header';
 
-export default function App() {
-  return (
-    <div>App</div>
-  )
+const getTodayString = () => {
+  const d = new Date()
+  const year = d.getFullYear()
+  const month = String(d.getMonth() + 1).padStart(2, '0')
+  const day = String(d.getDate()).padStart(2, '0')
+  return `${year}-${month}-${day}`
 }
 
+export default function App() {
+    
+  const [meds, setMeds] = useState(() => {
+    const saved = localStorage.getItem('medReminders')
+    return saved ? JSON.parse(saved) : []
+  })
+
+  const [name, setName] = useState('')
+  const [date, setDate] = useState(getTodayString())
+  const [time, setTime] = useState('')
+  const [lastAlarmed, setLastAlarmed] = useState('')
+  const [activeAlarmMed, setActiveAlarmMed] = useState(null)
+  const alarmStopRef = useRef(null)
+
+  useEffect(() => {
+    if ('Notification' in window && Notification.permission === 'default') {
+      Notification.requestPermission()
+    }
+  }, [])
+}
